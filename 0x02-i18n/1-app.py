41eb7e614c0bfd1f988c02728285ed886c1a76a1
bennet489 <bennetasare@outlook.com>
2024-04-10 14:39:25 +0300

    updated these

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,34 +1,27 @@
 #!/usr/bin/env python3
-'''
-Basic Flask Application'''
-from flask import Flask
-from flask import template
+""" Basic Babel setup"""
+
+from flask import Flask, render_template
 from flask_babel import Babel
 
 
 class Config(object):
-    '''
-    Application configuration class'''
+    """ Configuration for Babel"""
     LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = 'en'
-    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
-# Instantiation of application object
 app = Flask(__name__)
 app.config.from_object(Config)
+babel = Babel()
 
 
-# Wrapping application with Babel
-babel = Babel(app)
-
-
-@route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def index() -> str:
-    '''
-    Presents a basic html template'''
+    """Handles index route"""
     return render_template('1-index.html')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(port="5000", host="0.0.0.0", debug=True)
